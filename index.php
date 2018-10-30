73088924e622c0e2558d9036f3ab893825a9565b
mrmadhat <danny@websitesbydanny.co.uk>
2018-10-30 15:28:37 +0000

    Removed endpoints that are being transferred to sharpspring endpoint plugin

--- index.php
+++ index.php
@@ -20,37 +20,9 @@ namespace RelativeMarketing\Newsletter;
 
 defined('ABSPATH') or die();
 
-/**
- * Registers a new endpoint to allow us to connect to sharsprings
- * serverside and keep our secret key secret
- * 
- * url should take the form of:
- * 
- * www.example.com/wp-json/relativemarketing/v1/email/$email/name/$name
- * 
- */
 add_action( 'rest_api_init', __NAMESPACE__ . '\\init_endpoint' );
 
 function init_endpoint() {
-	register_rest_route( 
-		'relativemarketing/v1/newsletter',
-		'/email/(?P<email>[.\@a-zA-Z0-9]+)/name/(?P<name>[a-z0-9 %.\-]+)',
-		[
-			'methods' => 'GET',
-			'callback' => __NAMESPACE__ . '\\handle_newsletter_subscribe',
-			'args' => [
-				'name' => [
-					'validate_callback' => __NAMESPACE__ . '\\validate_name_string'
-				],
-				'email' => [
-					'validate_callback' => function( $param ) {
-						return is_email( $param );
-					}
-				],
-			]
-		] 
-	);
-
 	register_rest_route( 
 		'relativemarketing/v1/newsletter',
 		'/data',
@@ -59,87 +31,6 @@ function init_endpoint() {
 			'callback' => __NAMESPACE__ . '\\handle_get_data',
 		] 
 	);
-}
-
-/**
- * Attempt to add the email subscription to sharspring as a new lead
- */
-function handle_newsletter_subscribe( $params ) {
-
-	// Sort out name and email params
-	$split_name = explode( '%20', $params->get_param( 'name' ) );
-	$first_name = $split_name[0];
-	$last_name  = $split_name[ count( $split_name ) - 1 ];
-	$email      = $params->get_param('email');
-
-	// Data for the request
-	$account_id  = get_option( 'ss_api_key' );
-	$secret_key  = get_option( 'ss_secret_key' );
-
-	if ( empty($secret_key) || empty($account_id) ) {
-		return new \WP_Error('Either account ID or Secret Key is not set');
-	}
-
-	$request_id  = session_id();
-	$method      = 'createLeads';
-	
-	// If we have sharsprings(ss) tracking cookie then make sure it is attached
-	// to the lead when the lead is created
-	$tracking_id = array_key_exists( '__ss_tk', $_COOKIE ) ? $_COOKIE['__ss_tk'] : '';
-	$campaign_id = get_option( 'ss_campaign_id' );
-
-	// Format the params according to ss api requirements
-	$paramData = [ 
-		"objects" => [
-			[
-				"firstName"     => $first_name,
-				"lastName"      => $last_name,
-				"emailAddress"  => $email,
-				"trackingID"    => $tracking_id,
-				"campaignID"    => $campaign_id,
-			]
-		]
-	];
-
-	$data = json_encode( [
-		'method' => $method,
-		'params' => $paramData,
-		'id'     => $request_id,
-	] );
-
-	$query_string = http_build_query( ['accountID' => $account_id, 'secretKey' => $secret_key] );
-	$url = "https://api.sharpspring.com/pubapi/v1/?$query_string";
-
-	$ch = curl_init( $url );
-
-	curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "POST" );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, [
-		'Content-Type: application/json',
-		'Content-Length: ' . strlen( $data )
-	] );
-
-	$result = curl_exec( $ch );
-	curl_close( $ch );
-
-	return json_decode( $result  );
-
-}
-
-/**
- * Validates a given full name string
- */
-function validate_name_string( $name ) {
-	/**
-	 * ^          - Beginning of the line
-	 * [a-zA-Z- ] - Allowed characters
-	 * +          - At least one or more of the preceding
-	 * $          - End of line
-	 * 
-	 * Everything must match from the start and end to be true
-	 */
-	return (bool) preg_match( "/^[a-zA-Z- ]+$/", urldecode( $name ) );
 }
 
 /**
